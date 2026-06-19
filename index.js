de2dae01adef05f86ff7e1062e68a8291eec0d15
Parvez Patwary <95559291+parvezpatwaryy@users.noreply.github.com>
2026-06-19 20:42:30 +0600

    vew profile api added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { ObjectId } = require('mongodb');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
@@ -30,14 +31,28 @@ async function run() {
       res.send(result);
     });
     app.get('/tasks', async (req, res) => {
-      const page = parseInt(req.query.page) || 0;
-      const limit = parseInt(req.query.limit) || 9;
-      const result = await jobCollection.find()
-        .skip(page * limit)
-        .limit(limit)
-        .toArray();
-      res.send(result);
-    });
+      const email = req.query.email;
+      const page = parseInt(req.query.page);
+      const limit = parseInt(req.query.limit);
+
+      if (email) {
+        // ইমেইল থাকলে শুধু ওই ক্লায়েন্টের টাস্ক দিবে
+        const query = { client_email: email };
+        const result = await jobCollection.find(query).toArray();
+        res.send(result);
+      } else if (!isNaN(page) && !isNaN(limit)) {
+        // ইমেইল না থাকলে পেজিনেশন অনুযায়ী সব টাস্ক দিবে
+        const result = await jobCollection.find()
+          .skip(page * limit)
+          .limit(limit)
+          .toArray();
+        res.send(result);
+      } else {
+        // কোনো প্যারামিটার না থাকলে সব ডাটা দিবে
+        const result = await jobCollection.find().toArray();
+        res.send(result);
+      }
+    })
     app.get('/top-freelancers', async (req, res) => {
       const result = await freelancerCollection.find().sort({ rating: -1 }).limit(4).toArray();
       res.send(result);
@@ -46,6 +61,32 @@ async function run() {
       const totalTasks = await jobCollection.countDocuments();
       const totalFreelancers = await freelancerCollection.countDocuments();
       res.send({ totalTasks, totalFreelancers });
+    });
+
+
+    // index.js
+    app.get('/all-freelancers', async (req, res) => {
+      const result = await freelancerCollection.find().toArray();
+      res.send(result);
+    });
+
+
+
+    app.get('/freelancer/:id', async (req, res) => {
+      try {
+        const id = req.params.id;
+        // আইডিটি ObjectId তে কনভার্ট করে সার্চ করুন
+        const query = { _id: new ObjectId(id) };
+        const result = await freelancerCollection.findOne(query);
+
+        if (result) {
+          res.send(result);
+        } else {
+          res.status(404).send({ message: "Freelancer not found!" });
+        }
+      } catch (error) {
+        res.status(500).send({ message: "Invalid ID format or Server Error" });
+      }
     });
 
     await client.db("admin").command({ ping: 1 });
