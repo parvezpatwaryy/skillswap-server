78999b8966ba89ad2df6df761c49e9326e61b9d8
Parvez Patwary <95559291+parvezpatwaryy@users.noreply.github.com>
2026-06-19 12:52:37 +0600

    tasks api complite

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const app = express()
 const port = 5000
 require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.get('/', (req, res) => {
@@ -10,8 +14,6 @@ app.get('/', (req, res) => {
 
 
 const uri = process.env.MONGO_DB_URI;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -22,14 +24,27 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const database = client.db("skillswap-db");
+    const jobCollection = database.collection("tasks");
+
+    app.get('/tasks', async (req, res) => {
+      // ফ্রন্টএন্ড থেকে page এবং limit প্যারামিটার নেবে
+      const page = parseInt(req.query.page) || 0;
+      const limit = parseInt(req.query.limit) || 9; // ডিফল্ট ৯টি ডেটা
+
+      const result = await jobCollection.find()
+        .skip(page * limit)
+        .limit(limit)
+        .toArray();
+
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
